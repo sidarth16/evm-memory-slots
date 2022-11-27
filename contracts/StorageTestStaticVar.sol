282e7843be5c5b0fe2247167b95705e03eea5a16
sidarth16 <ssidarth1999@gmail.com>
2022-11-27 23:07:24 +0530

    upd var name

--- contracts/StorageTestStaticVar.sol
+++ contracts/StorageTestStaticVar.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.9;
 
 contract StorageTestStaticVar {
-    uint8 public a = 7; //0
-    uint16 public b =10; //0
-    address public d = 0xbE03bCA4D65A86114668697867982Ecfc76f15F8; //0  //160 bits
-    bool public bb = true; //0
-    uint64 public c = 15; //0
-    uint256 public e = 200; //1
-    uint8 public f = 40; //2
-    uint256 public g = 789; //3
+    uint8 private a = 7; //0
+    uint16 private b =10; //0
+    address public c = 0xbE03bCA4D65A86114668697867982Ecfc76f15F8; //0  //160 bits
+    bool public d = true; //0
+    uint64 public e = 15; //0
+    uint256 public f = 200; //1
+    uint8 public g = 40; //2
+    uint256 public h = 789; //3
 }
